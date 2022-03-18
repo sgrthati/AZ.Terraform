0ec766a6bc911618cd03fe9cc52e94121f57fa74
Sagar <55286233+SagarThati@users.noreply.github.com>
2022-03-18 13:51:11 +0530

    Update SIngleVM.tf

--- SIngleVM.tf
+++ SIngleVM.tf
@@ -11,17 +11,17 @@ terraform {
 provider "azurerm" {
   features {}
 
-  subscription_id = "aaca85ab-ad78-4c79-8102-031f7ae1a0dc"
-  client_id       = "910d9557-1911-41eb-9fea-9a5353486bb5"
-  client_secret   = "shA7Q~wgujZyHE0lWjYRl8CD4XSy1n1Lf5H_w"
-  tenant_id       = "af794339-2fdc-421d-ac0a-f3824494051e"
+  subscription_id = "00000000-0000-0000-0000-000000000000"
+  client_id       = "00000000-0000-0000-0000-000000000000"
+  client_secret   = "00000000-0000-0000-0000-000000000000"
+  tenant_id       = "00000000-0000-0000-0000-000000000000"
 }
 resource "azurerm_resource_group" "RG1" {
-  name     = "srisagar"
+  name     = "SRI"
   location = "East US"
 }
 resource "azurerm_network_security_group" "NSG" {
-    name = "Sagar-NSG"
+    name = "SRI-NSG"
     location = azurerm_resource_group.RG1.location
     resource_group_name = azurerm_resource_group.RG1.name
 
@@ -49,38 +49,38 @@ resource "azurerm_network_security_group" "NSG" {
   }
 }
 resource "azurerm_virtual_network" "VNET" {
-  name                = "SAGAR-VNET"
+  name                = "SRI-VNET"
   location            = azurerm_resource_group.RG1.location
   resource_group_name = azurerm_resource_group.RG1.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
 resource "azurerm_subnet" "SUBNET1" {
-  name                 = "internal"
+  name                 = "SRI-SN1"
   resource_group_name  = azurerm_resource_group.RG1.name
   virtual_network_name = azurerm_virtual_network.VNET.name
   address_prefixes      = ["10.0.2.0/24"]
 }
 resource "azurerm_public_ip" "PIP" {
-  name                = "SAGAR-PIP"
+  name                = "SRI-PIP"
   resource_group_name = azurerm_resource_group.RG1.name
   location            = azurerm_resource_group.RG1.location
   allocation_method   = "Static"
 }
 resource "azurerm_network_interface" "NIC" {
-  name                = "SAGAR-NIC"
+  name                = "SRI-NIC"
   location            = azurerm_resource_group.RG1.location
   resource_group_name = azurerm_resource_group.RG1.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "IPCONFIG1"
     subnet_id                     = azurerm_subnet.SUBNET1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.PIP.id
   }
 }
 resource "azurerm_windows_virtual_machine" "VM1" {
-  name                = "SAGARVM"
+  name                = "SRIVM"
   resource_group_name = azurerm_resource_group.RG1.name
   location            = azurerm_resource_group.RG1.location
   size                = "Standard_B1s"
