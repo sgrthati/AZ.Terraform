37d00c54ab5009eaf5c0d5050a10bd3ad8c5d682
thatisagar <sgrthati@gmail.com>
2022-03-20 11:26:36 +0000

    MultiVM script did succesfully

--- MultiVM/MultiVMVariables.tf
+++ MultiVM/MultiVMVariables.tf
@@ -1,1 +1,28 @@
-
+variable "resource_groups" {
+  description = "(Required) A list of Azure Resource Groups with locations and tags"
+  default = "SRI"
+}
+variable "location" {
+  default = "Central India"
+}
+variable "NSG" {
+  default = "SRI-NSG"
+}
+variable "VNET" {
+  default = "SRI-VNET"
+}
+variable "subnet" {
+  default = "SRI-SUBNET"
+}
+variable "publicIP" {
+  default = "SRI-PIP"
+}
+variable "NIC" {
+  default = "SRI-NIC"
+}
+variable "VM" {
+  default = "SRIVM"
+}
+variable "VMcount" {
+  default = 2
+}
