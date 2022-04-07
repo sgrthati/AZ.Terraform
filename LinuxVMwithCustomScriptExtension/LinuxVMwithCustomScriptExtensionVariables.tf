fe26406675ee8b0c30b03da441677d8024163816
Sagar <101870480+thatisagar@users.noreply.github.com>
2022-04-07 13:27:12 +0530

    FINAL

---
+++ LinuxVMwithCustomScriptExtension/LinuxVMwithCustomScriptExtensionVariables.tf
@@ -0,0 +1,37 @@
+variable "resource_groups" {
+  description = "(Required) A list of Azure Resource Groups with locations and tags"
+  default = "SRI"
+}
+variable "location" {
+  default = "Central India"
+}
+variable "NSG" {
+  default = "SRI-NSG"
+}
+variable "VNET" {
+  default = "SRI-VNET"
+}
+variable "subnet" {
+  default = "SRI-SUBNET"
+}
+variable "publicIP" {
+  default = "SRI-PIP"
+}
+variable "NIC" {
+  default = "SRI-NIC"
+}
+variable "VM" {
+  default = "SRIVM"
+}
+variable "VMcount" {
+  default = 2
+}
+variable "FileUri" {
+  default = "https://sagarsri1.blob.core.windows.net/scripts/java.sh"
+}
+variable "StorageAccountName" {
+  default = "sagarsri1"
+}
+variable "StorgaeAccountKey" {
+  default = ">STORAGE ACCOUNT KEY<"
+}
