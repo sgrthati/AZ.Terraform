3e4f086d19b64e5018c0813cb44b02264da0961e
sgrthati <sgrthati@gmail.com>
2024-07-02 14:49:37 +0530

    missed VNET module added

---
+++ modules/generic_resources/vnet/main.tf
@@ -0,0 +1,48 @@
+data "azurerm_resource_group" "main" {
+  name = "${var.resource_group_name}"
+}
+
+locals {
+  resource_group_name = "${data.azurerm_resource_group.main.name}"
+  location            = "${var.location != "" ? var.location : data.azurerm_resource_group.main.location}"
+  VNET_name = "${var.VNET_name != "" ? var.VNET_name : "${var.resource_group_name}-vnet"}"
+  subnet_name = "${var.subnet_name != "" ? var.subnet_name : "${var.resource_group_name}-vnet-subnet"}"
+  nsg_name = "${var.nsg_name != "" ? var.nsg_name : "${var.resource_group_name}-vnet-nsg"}"
+}
+
+resource "azurerm_virtual_network" "VNET" {
+  name                = local.VNET_name
+  address_space       = ["10.1.0.0/16"]
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+}
+resource "azurerm_subnet" "SUBNET1" {
+  name                 = local.subnet_name
+  resource_group_name  = local.resource_group_name
+  virtual_network_name = azurerm_virtual_network.VNET.name
+  address_prefixes       = ["10.1.1.0/24"]
+}
+
+resource "azurerm_network_security_group" "NSG" {
+  name                = local.nsg_name
+  location            = local.location
+  resource_group_name = local.resource_group_name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
+  subnet_id                 = azurerm_subnet.SUBNET1.id
+  network_security_group_id = azurerm_network_security_group.NSG.id
+}
---
+++ modules/generic_resources/vnet/variables.tf
@@ -0,0 +1,21 @@
+variable "VNET_name" {
+    default = ""
+}
+
+variable "subnet_name" {
+    default = ""
+}
+
+variable "RG_name" {
+    default = ""
+}
+
+variable "nsg_name" {
+    default = ""
+}
+variable "resource_group_name" {
+    default = ""
+}
+variable "location" {
+    default = ""
+}
