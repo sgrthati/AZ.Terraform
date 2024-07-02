8c6519d03e701a75dbf6bcf668b86c104dd60ccb
sgrthati <sgrthati@gmail.com>
2024-07-02 20:53:13 +0530

    modified

--- modules/generic_resources/linux_vm/output.tf
+++ modules/generic_resources/linux_vm/output.tf
@@ -2,5 +2,5 @@ output "admin_username" {
   value = "${var.admin_username}"
 }
 output "vm_public_ip" {
-  value = "${for pip in azurerm_public_ip.pip : pip.ip_address}"
+  value = [for pip in azurerm_public_ip.pip : pip.ip_address]
 }
