e970e5f16991628f3f54893c0d69273ad00b9de1
sgrthati <sgrthati@gmail.com>
2024-07-06 14:22:28 +0530

    vnet nsg rules modified

--- modules/generic_resources/vnet/main.tf
+++ modules/generic_resources/vnet/main.tf
@@ -24,21 +24,24 @@ resource "azurerm_subnet" "SUBNET1" {
   address_prefixes       = ["10.1.1.0/24"]
 }
 
-resource "azurerm_network_security_group" "NSG" {
+resource "azurerm_network_security_group" "NSG" {       
   name                = local.nsg_name
   location            = local.location
   resource_group_name = local.resource_group_name
 
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+  dynamic "security_rule" {
+    for_each = var.allowed_inbound_ports
+    content {
+      name                       = security_rule.key
+      priority                   = security_rule.value.priority
+      direction                  = "Inbound"
+      access                     = "Allow"
+      protocol                   = "Tcp"
+      source_port_range          = "*"
+      destination_port_range     = security_rule.value.port
+      source_address_prefix      = "*"
+      destination_address_prefix = "*"
+    }
   }
 }
 
--- modules/generic_resources/vnet/variables.tf
+++ modules/generic_resources/vnet/variables.tf
@@ -19,3 +19,24 @@ variable "resource_group_name" {
 variable "location" {
     default = ""
 }
+variable "allowed_inbound_ports" {
+  description = "Map of allowed inbound ports with priorities"
+  type = map(object({
+    port     = string
+    priority = number
+  }))
+  default = {
+    "AllowSSH" = {
+      port     = "22"
+      priority = 100
+    }
+    "AllowHTTP" = {
+      port     = "80"
+      priority = 110
+    }
+    "AllowHTTPS" = {
+      port     = "443"
+      priority = 120
+    }
+  }
+}
