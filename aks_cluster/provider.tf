11632d1ae2baab8b71507a1a41821b24c2852363
sgrthati <sgrthati@gmail.com>
2024-08-28 21:52:54 +0530

    sub details added

--- aks_cluster/provider.tf
+++ aks_cluster/provider.tf
@@ -6,9 +6,10 @@ terraform {
     }
   }
 }
-
+data "azurerm_client_config" "current" {}
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
+  subscription_id = data.azurerm_client_config.current.subscription_id
 }
 provider "azuread" {}
